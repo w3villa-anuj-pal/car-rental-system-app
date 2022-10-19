58920cd9aedd1d286eedaa62ca4eaf6e556db712
w3villa-anuj-pal <anuj.pal@w3villa.com>
2022-10-20 02:33:53 +0530

    confirmation mail on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,19 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: "anuj-car-rental-system.herokuapp.com", protocol: "https" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               "anuj-car-rental-system.herokuapp.com",
+    user_name:            Rails.application.credentials.dig(:google_smtp, :email),
+    password:             Rails.application.credentials.dig(:google_smtp, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
