ccc719e824051011225e85a62476ed98901dc073
w3villa-anuj-pal <anuj.pal@w3villa.com>
2022-10-20 16:18:29 +0530

    add sessions table and update users schema

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  
+  def facebook
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
+      if @user.persisted?
+        sign_in_and_redirect @user
+        set_flash_message(:notice, :sucess, kind: 'Facebook') if is_navigational_format?
+      else
+        flash[:error] = "There was a problem signing you in through Facebook. Please try again later."
+        redirect_to new_user_registration_url
+      end
+  end
+
+  def failure
+    flash[:error] = "There was a problem signing you in. Please try again later."
+    redirect_to new_user_registration_url
+  end
+end
